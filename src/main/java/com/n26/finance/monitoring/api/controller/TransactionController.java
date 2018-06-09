2b238ce13077ad35cd1ed8768c3bac2a50fae08b
Danilo Araújo Silva <silva.danilo.araujo@gmail.com>
2018-06-09 14:59:51 -0300

    Making transaction controller pass.

--- src/main/java/com/n26/finance/monitoring/api/controller/TransactionController.java
+++ src/main/java/com/n26/finance/monitoring/api/controller/TransactionController.java
@@ -1,7 +1,23 @@
 package com.n26.finance.monitoring.api.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+
 /**
  *
  */
+@Controller("transactionController")
+@CrossOrigin(origins = "*")
 public class TransactionController {
+
+	/**
+	 *
+	 */
+	@PostMapping("/transaction")
+	public ResponseEntity<Void> insertTransaction() {
+		return new ResponseEntity<Void>(HttpStatus.CREATED);
+	}
 }
