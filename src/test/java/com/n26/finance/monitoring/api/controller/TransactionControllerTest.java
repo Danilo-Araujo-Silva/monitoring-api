607435b55fc93ae85304c667fd4fcaff82c34e2f
Danilo Araújo Silva <silva.danilo.araujo@gmail.com>
2018-06-09 14:53:22 -0300

    Creating firest transaction controller test, not passing.

--- src/test/java/com/n26/finance/monitoring/api/controller/TransactionControllerTest.java
+++ src/test/java/com/n26/finance/monitoring/api/controller/TransactionControllerTest.java
@@ -1,7 +1,62 @@
 package com.n26.finance.monitoring.api.controller;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
 /**
  *
  */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class TransactionControllerTest extends AbstractControllerTest {
+
+	/**
+	 *
+	 */
+	@Autowired
+	private MockMvc mockMvc;
+
+	/**
+	 *
+	 */
+	private String endpointPrefix = "/transaction";
+
+	/**
+	 *
+	 */
+	@Before
+	public void setup() {
+	}
+
+	/**
+	 *
+	 */
+	@Test
+	public void insertTransaction() throws Exception {
+		String input = "";
+
+		Integer status = mockMvc
+			.perform(
+				post(endpointPrefix)
+					.content(input)
+					.contentType(MediaType.APPLICATION_JSON)
+					.accept(MediaType.APPLICATION_JSON)
+			)
+			.andReturn()
+			.getResponse()
+			.getStatus();
+
+		assert status.equals(HttpStatus.CREATED.value());
+	}
 }
