ffa3e2668d97e46ba8d7d5493768babbfb3cc69e
Danilo Araújo Silva <silva.danilo.araujo@gmail.com>
2018-06-09 12:01:39 -0300

    Initial commit.

---
+++ src/main/java/com/n26/finance/monitoring/api/N26FinanceMonitoringApiApplication.java
@@ -0,0 +1,12 @@
+package com.n26.finance.monitoring.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class N26FinanceMonitoringApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(N26FinanceMonitoringApiApplication.class, args);
+	}
+}
---
+++ src/main/java/com/n26/finance/monitoring/api/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.n26.finance.monitoring.api;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(N26FinanceMonitoringApiApplication.class);
+	}
+
+}
